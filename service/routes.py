c9b4058433e5e9a99d8b449f1aba5219935036cf
niluse <niluse02@gmail.com>
2022-11-27 14:10:22 -0500

    Added badge for GitHub Actions

--- service/routes.py
+++ service/routes.py
@@ -60,6 +60,8 @@ def create_accounts():
     ######################################################################
     # LIST ALL ACCOUNTS
     ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -94,9 +96,9 @@ def get_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-    ######################################################################
-    # UPDATE AN EXISTING ACCOUNT
-    ######################################################################
+######################################################################
+# UPDATE AN EXISTING ACCOUNT
+######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
