77f797087bd1e32665779f71f2797fc74850170f
niluse <95444944+niluse@users.noreply.github.com>
2022-11-27 22:25:57 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -119,6 +119,8 @@ def update_accounts(account_id):
     ######################################################################
     # DELETE AN ACCOUNT
     ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
